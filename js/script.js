76c8648b49e13ab869ac4537becad7891ed8553c
xgoldzin <gustafera34@gmail.com>
2024-10-09 15:02:16 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let listaAlunos = [];
+let alunoEditIndex = null; // Armazena o índice do aluno em edição
+
+function cadastrarAluno() {
+    let nome = document.getElementById("nome").value;
+    let dataNasc = document.getElementById("dataNasc").value;
+    let curso = document.getElementById("curso").value;
+}
